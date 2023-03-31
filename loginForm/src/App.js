a6211258d03b813abdeac5d7cfca9d17385c1329
Dux <kartikmadaan1907@gmail.com>
2023-03-31 16:16:02 +0530

    Optimisation

--- loginForm/src/App.js
+++ loginForm/src/App.js
@@ -1,29 +1,40 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import React,{ useState } from "react";
 import './index.css'
 export default function App() {
-  const [initial,final] = useState();
-  const [heading, newhg] = useState();
-  const [last, lastNew] = useState();
-  const [full, fullNew] = useState();
+  const [initial, final] = useState({
+    fname: '',
+    lname: ''
+  });
   const change = (e) => {
-    final(e.target.value)
-  }
-  const changeOne = (e) => {
-    lastNew(e.target.value)
-  }
+    let n = e.target.name
+    
+    final((random) => {
+      if (n === 'fName') {
+        return {
+          fname: e.target.value,
+          lname: random.lname
+        }
+      }
+      else {
+        return {
+          fname: random.fname,
+          lname: e.target.value
+        }
+      }
+        })
+   }
+  
   const set = (event) => {
     event.preventDefault()  // to prevent of refeshing the page as when the submit button is clicked in a form,the page is refreshed immediately and automatically.
-    newhg(initial)
-    fullNew(last)
-    final('')
-    lastNew('')
+    alert('saved')
   }
   return(
     <>
-      <h2 className='mb-3'>Hello {heading} {full} </h2>
+      <h2 className='mb-3'>Hello {initial.fname} {initial.lname} </h2>
       <form>
-      <input id='input' type='text' placeholder='Enter your first name' value={initial} onChange={change} />
-      <input id='input' type='text' placeholder='Enter your last name' value={last} onChange={changeOne} />
+      <input id='input' type='text' placeholder= 'Enter your first name' name = 'fName' onChange={change} />
+      <input id='input' type='text' placeholder='Enter your last name'  name = 'lName' onChange={change} />
       <br/>
         <button onClick={set} type='submit' className="btn btn-primary mt-3">Click to submit</button>
         </form>
