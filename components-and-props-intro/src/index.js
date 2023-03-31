34d3f233ecd831be8871f50a29994e3e95d63415
Dux <kartikmadaan1907@gmail.com>
2023-03-31 16:16:28 +0530

    Nothing serious

--- components-and-props-intro/src/index.js
+++ components-and-props-intro/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Data from './Data'
 import Card, { center } from './App'
-import styles from './index.css'
 
 ReactDOM.render(
   <>
